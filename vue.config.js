bce9aa0575cf0dde4cbfe7be84cbd86a0ea47353
Harris <harrisjose@outlook.com>
2020-01-25 11:36:01 +0530

    Add inline svg

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,26 @@
+const path = require('path')
+let isDev = process.env.NODE_ENV === 'development'
+
 module.exports = {
   transpileDependencies: ['vuetify'],
+  configureWebpack: config => {
+    if (isDev) {
+      config.devtool = 'eval-source-map'
+
+      config.output.devtoolModuleFilenameTemplate = info =>
+        info.resourcePath.match(/^\.\/\S*?\.vue$/)
+          ? `webpack-generated:///${info.resourcePath}?${info.hash}`
+          : `webpack-vue:///${info.resourcePath}`
+
+      config.output.devtoolFallbackModuleFilenameTemplate =
+        'webpack:///[resource-path]?[hash]'
+    }
+
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      ...{
+        assets: path.resolve(__dirname, 'src/assets'),
+      },
+    }
+  },
 }
