58434b21cb97c5ca50126726ea849091d22d34cb
praveen kumar <praveen.dinesh6@gmail.com>
2020-01-25 11:18:39 +0530

    Added vuetify

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,5 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" class="m-auto" />
-    <HelloWorld msg="Welcome to Your Vue.js App" class="text-red-600" />
-  </div>
+  <HelloWorld msg="Welcome to Your Vue.js App" class="text-red-600" />
 </template>
 
 <script>
